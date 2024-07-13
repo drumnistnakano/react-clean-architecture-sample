47debf27498a4559ebc2623213e8e13002dad5f6
drumnistnakano <30113636+drumnistnakano@users.noreply.github.com>
2024-07-14 01:56:41 +0900

    add vitest

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: "./",
+  plugins: [react(), tsconfigPaths()],
+  // @see https://vitest.dev/guide/#configuring-vitest
+  test: {
+    include: ["src/**/*.test.{ts,tsx}"],
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["html"],
+      reportsDirectory: "./coverage",
+    },
+  },
+  cacheDir: "node_modules/.cache/vitest",
 });
