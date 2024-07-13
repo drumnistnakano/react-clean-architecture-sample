1a789254855874065a5c11a7a8ae8c26f08c7ebc
drumnistnakano <30113636+drumnistnakano@users.noreply.github.com>
2024-07-14 02:37:43 +0900

    Add Domain Model(Album and Photo Entities)

---
+++ photo-album/src/core/domain/entities/album.ts
@@ -0,0 +1,19 @@
+export interface Album {
+  /**
+   * ユーザーID
+   * @example 1
+   */
+  userId: number;
+
+  /**
+   * アルバムID
+   * @example 1
+   */
+  id: number;
+
+  /**
+   * アルバムのタイトル
+   * @example "quidem molestiae enim"
+   */
+  title: string;
+}
---
+++ photo-album/src/core/domain/entities/photo.ts
@@ -0,0 +1,31 @@
+export interface Photo {
+  /**
+   * アルバムID
+   * @example 1
+   */
+  albumId: number;
+
+  /**
+   * 写真ID
+   * @example 1
+   */
+  id: number;
+
+  /**
+   * 写真のタイトル
+   * @example "accusamus beatae ad facilis cum similique qui sunt"
+   */
+  title: string;
+
+  /**
+   * 写真のURL
+   * @example "https://via.placeholder.com/600/92c952"
+   */
+  url: string;
+
+  /**
+   * サムネイルのURL
+   * @example "https://via.placeholder.com/150/92c952"
+   */
+  thumbnailUrl: string;
+}
