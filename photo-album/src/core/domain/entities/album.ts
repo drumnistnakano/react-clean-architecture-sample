32dceaf468d0a57d1f6d330c2208dea18b18e58e
drumnistnakano <30113636+drumnistnakano@users.noreply.github.com>
2024-07-14 10:54:40 +0900

    fix album and photo spell checker

--- photo-album/src/core/domain/entities/album.ts
+++ photo-album/src/core/domain/entities/album.ts
@@ -1,18 +1,22 @@
 export interface Album {
   /**
    * ユーザーID
+   *
    * @example 1
    */
   userId: number;
 
   /**
    * アルバムID
+   *
    * @example 1
    */
   id: number;
 
   /**
    * アルバムのタイトル
+   *
+   * cspell:disable-next-line
    * @example "quidem molestiae enim"
    */
   title: string;
--- photo-album/src/core/domain/entities/photo.ts
+++ photo-album/src/core/domain/entities/photo.ts
@@ -1,30 +1,36 @@
 export interface Photo {
   /**
    * アルバムID
+   *
    * @example 1
    */
   albumId: number;
 
   /**
    * 写真ID
+   *
    * @example 1
    */
   id: number;
 
   /**
    * 写真のタイトル
+   *
+   * cspell:disable-next-line
    * @example "accusamus beatae ad facilis cum similique qui sunt"
    */
   title: string;
 
   /**
    * 写真のURL
+   *
    * @example "https://via.placeholder.com/600/92c952"
    */
   url: string;
 
   /**
    * サムネイルのURL
+   *
    * @example "https://via.placeholder.com/150/92c952"
    */
   thumbnailUrl: string;
